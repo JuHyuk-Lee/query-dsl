7d4f270ae0261be30236d9ef4dda2e94a887b23d
LeeJuHyuk <dlwngur1535@gmail.com>
2020-08-19 11:25:12 +0900

    no message

--- query-dsl/src/test/java/lee/study/querydsl/QueryDslApplicationTests.java
+++ query-dsl/src/test/java/lee/study/querydsl/QueryDslApplicationTests.java
@@ -1,13 +1,41 @@
 package lee.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lee.study.querydsl.entity.Hello;
+import lee.study.querydsl.entity.QHello;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Commit // @Transactional에 의해서 rollback가 되기 때문에 설정
 class QueryDslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
---
+++ query-dsl/src/test/java/lee/study/querydsl/QueryDslBasicTests.java
@@ -0,0 +1,94 @@
+package lee.study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lee.study.querydsl.entity.Member;
+import lee.study.querydsl.entity.QMember;
+import lee.study.querydsl.entity.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static lee.study.querydsl.entity.QMember.*;
+
+@SpringBootTest
+@Transactional
+public class QueryDslBasicTests {
+
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory queryFactory ;
+
+    @BeforeEach
+    public void beforeEach() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        Member findMember = em.createQuery("select m from Member m where m.username = :username"
+                , Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(QMember.member)
+                .where(QMember.member.username.eq("member1")
+                        .and(QMember.member.age.eq(10)))
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void searchAndParam() {
+        // search()처럼 and()로 묶거나 , 로 사용가능
+        Member findMember = queryFactory
+                .selectFrom(QMember.member)
+                .where(
+                        QMember.member.username.eq("member1"),
+                        (QMember.member.age.eq(10))
+                )
+                .fetchOne();
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
